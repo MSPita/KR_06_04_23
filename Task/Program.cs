e4b78b75110b84f5229f9089f531014b9d7e6ae4
Maria Pita <mahoni@bk.ru>
2023-04-08 15:46:01 +0300

    Task

---
+++ Task/Program.cs
@@ -0,0 +1,50 @@
+﻿Console.Write("Введите количество строк массива: ");
+int m = int.Parse(Console.ReadLine()!);
+string[] arr = new string[m];
+
+void Array(string[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.WriteLine($"Введите {i + 1} строку ");
+        arr[i] = Console.ReadLine()!;
+
+    }
+
+}
+string[] Master(string[] arr)
+{
+    int n = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i].Length <= 3)
+            n++;
+    }
+
+    string[] newarr = new string[n];
+    int j = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i].Length <= 3)
+        {
+            newarr[j] = arr[i];
+            j++;
+        }
+    }    
+    return newarr;
+}
+
+void Print(string[] arr)
+{
+    Console.Write("[");
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write($"{arr[i]} ");
+    }
+    Console.Write("]");
+}
+Array(arr);
+Print(arr);
+
+string[] newarr2 = Master(arr);
+Print(newarr2);
